eaefd46bfe7357028a3c81e81854c2c652212973
Aglalrizal <rizalaglalfaoji017@gmail.com>
2023-11-09 11:48:58 +0700

    Perbaikan

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-function cekKolomKosong() {
-    var kolomInput = document.getElementById('email'); // Ganti 'namaKolom' dengan ID kolom yang ingin Anda periksa
-  
-    if (kolomInput.value.trim() === '') {
-      // Kolom kosong
-      alert('Kolom harus diisi');
-      return false; // Formulir tidak akan disubmit
+document.getElementById("btn1").addEventListener("click", function () {
+    toggleInfo("info1", "btn1");
+});
+
+document.getElementById("btn2").addEventListener("click", function () {
+    toggleInfo("info2", "btn2");
+});
+
+document.getElementById("btn3").addEventListener("click", function () {
+    toggleInfo("info3", "btn3");
+});
+
+function toggleInfo(id, buttonId) {
+    var infoElement = document.getElementById(id);
+    var buttonElement = document.getElementById(buttonId);
+
+    if (infoElement.style.display === "none") {
+        infoElement.style.display = "block";
+        buttonElement.textContent = "Less Info";
+    } else {
+        infoElement.style.display = "none";
+        buttonElement.textContent = "More Info";
     }
-  
-    // Kolom sudah diisi
-    return true; // Formulir akan disubmit
-  }
+}
+
+function cekKolomKosong() {
+    var kolomInput = document.getElementById('email');
+
+    if (kolomInput.value.trim() === '') {
+        kolomInput.style.border = "1px solid red";
+        alert('Kolom harus diisi');
+        return false; // Formulir tidak akan disubmit
+    } else {
+        alert('Form Submitted')
+        return true;
+    }
+}
